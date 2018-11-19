19f938280b1db2e0075e0412b8085b1ce61a7ca7
Mario2495 <mario.cancel1@upr.edu>
2018-11-19 14:35:24 -0400

    Cabios al modulo del postjs

--- backend/models/post.js
+++ backend/models/post.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 //define los campos y el tipo que vas a utlizar
 const postSchema = mongoose.Schema({
       title:{ type: String, required: true},
-      content:{ type: String, required: true},
+      content:{ type: String, required: false},
       songs:{ type: String, required: false},
-      date:{ type: String, required: false}
+      date:{ type: String, required: true}
 
 });
 
