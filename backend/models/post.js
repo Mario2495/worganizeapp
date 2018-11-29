d1ad7b5174521051837185466a3c83be4e2b1e78
Leilanie <leilanie.morales@upr.edu>
2018-11-28 21:54:35 -0400

    Authentication
    hasta donde se, se supone que funcione. No lo llegue a probar

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-//define los campos y el tipo que vas a utlizar
+
 const postSchema = mongoose.Schema({
       title: { type: String, required: true},
       content: { type: String, required: false},
       songs: { type: String, required: false},
       date: { type: String, required: true},
       imagePath: { type: String, required: true }
-
 });
 
-module.exports = mongoose.model('Post',postSchema);
+module.exports = mongoose.model('Post', postSchema);
 
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -31,6 +32,7 @@ const storage = multer.diskStorage({
 });
 
 router.post("",
+checkAuth,
 multer({storage: storage}).single("image"),
 (req, res, next) => {
   const url = req.protocol + "://" + req.get("host");
@@ -53,7 +55,9 @@ multer({storage: storage}).single("image"),
  }
 );
 
-router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+router.put("/:id",
+checkAuth,
+multer({storage: storage}).single("image"), (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + "://" + req.get("host");
@@ -108,7 +112,7 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
     res.status(200).json({message: "Post deleted!"});
